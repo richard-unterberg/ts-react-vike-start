4f0b5234a497f4214fa324b6883a8213b2cf5c98
richard-unterberg <richard-unterberg@users.noreply.github.com>
2024-01-01 01:31:27 +0000

    deploy: ae34ffbfe0c57d1b1e6c4abada8c03b5e56e8b22

--- pages/index/+config.h.ts
+++
@@ -1,5 +0,0 @@
-export default {
-  title: 'TypeScript + React + Vike | ts-react-vike-start',
-  description:
-    'Featuresome template to provide an additional setup to get TS-React working in Vite + Vike (Vite SSR) with HMR and handful of strict ESLint rules. I want to add to work with properly in my side projects.',
-}
