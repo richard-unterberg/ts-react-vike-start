197ee2aa2e104a486ac44279ab8add0b15fbfc14
richard-unterberg <richard-unterberg@users.noreply.github.com>
2024-01-15 20:29:36 +0000

    deploy: 627c18afe5311a2314300aa8837b6046d2b191ff

---
+++ assets/entries/pages_index.82xpb2fo.js
@@ -0,0 +1,17 @@
+import{F as o,j as e,B as t,I as i,c as r,i as s,a as l,b as d}from"../chunks/chunk-IHE30XRr.js";const n=o.h3`
+  flex
+  gap-2
+  justify-between
+  items-center
+  text-light
+  text-2xl
+  mb-3
+  font-bold
+  whitespace-nowrap
+`,a=o.code`
+  font-mono
+  p-1
+  inline-block
+  text-warning
+  bg-dark
+`,c=()=>e.jsxs(e.Fragment,{children:[e.jsx("h2",{className:"text-3xl mb-4",id:"usage",children:"Usage"}),e.jsxs("div",{className:"grid grid-cols-1 sm:grid-cols-2 gap-5",children:[e.jsxs(t,{children:[e.jsxs(n,{children:["Installation",e.jsx(i,{icon:r.MonitorDown,className:"text-grayDark"})]}),e.jsxs("p",{children:["clone repo and: ",e.jsx("br",{}),"run ",e.jsx(a,{children:"npm install"})," in root"]})]}),e.jsxs(t,{children:[e.jsxs(n,{children:["Local Dev ",e.jsx(i,{icon:r.PencilRuler,className:"text-grayDark"})]}),e.jsxs("p",{children:["Start local dev server with ",e.jsx("br",{}),e.jsx(a,{children:"npm run dev"})]})]}),e.jsxs(t,{children:[e.jsxs(n,{children:["Build ",e.jsx(i,{icon:r.Sailboat,className:"text-grayDark"})]}),e.jsxs("p",{children:["build production ready copy to ",e.jsx("span",{className:"font-mono",children:"dist"})," folder:",e.jsx("br",{}),e.jsx(a,{children:"npm run build"})]})]}),e.jsxs(t,{children:[e.jsxs(n,{children:["Preview ",e.jsx(i,{icon:r.PackageCheck,className:"text-grayDark"})]}),e.jsxs("p",{children:["Preview the build from ",e.jsx("span",{className:"font-mono",children:"dist"})," folder: ",e.jsx("br",{}),e.jsx(a,{children:"npm run preview"})]})]})]})]}),x=Object.freeze(Object.defineProperty({__proto__:null,default:c},Symbol.toStringTag,{value:"Module"})),h=[{configName:"onRenderClient",importPath:"/renderer/+onRenderClient.tsx",isValueFile:!0,exportValues:s},{configName:"onPageTransitionStart",importPath:"/renderer/+onPageTransitionStart.ts",isValueFile:!0,exportValues:l},{configName:"onPageTransitionEnd",importPath:"/renderer/+onPageTransitionEnd.ts",isValueFile:!0,exportValues:d},{configName:"Page",importPath:"/pages/index/+Page.tsx",isValueFile:!0,exportValues:x}],u={onBeforeRenderEnv:{definedAt:{isComputed:!0},valueSerialized:"null"},dataEnv:{definedAt:{isComputed:!0},valueSerialized:"null"},hydrationCanBeAborted:{definedAt:{filePathToShowToUser:"/renderer/+config.h.ts",fileExportPathToShowToUser:["default","hydrationCanBeAborted"]},valueSerialized:"true"},title:{definedAt:{filePathToShowToUser:"/pages/index/+config.h.ts",fileExportPathToShowToUser:["default","title"]},valueSerialized:'"TypeScript + React + Vike | ts-react-vike-start"'},description:{definedAt:{filePathToShowToUser:"/pages/index/+config.h.ts",fileExportPathToShowToUser:["default","description"]},valueSerialized:'"Featuresome template to provide an additional setup to get TS-React working in Vite + Vike (Vite SSR) with HMR and handful of strict ESLint rules. I want to add to work with properly in my side projects."'}};export{h as configValuesImported,u as configValuesSerialized};
